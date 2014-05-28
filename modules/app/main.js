c654f40afd7f895776c9f4a169312f9eea00dea0
hefangshi <hefangshi@gmail.com>
2014-05-28 15:38:31 +0800

    first commit

---
+++ modules/app/main.js
@@ -0,0 +1,15 @@
+var Backbone, Workspace, app;
+
+Backbone = require('backbone');
+
+Backbone.$ = require('jquery');
+
+app = require('app/views/app');
+
+Workspace = require('app/routers/router');
+
+new Workspace();
+
+Backbone.history.start();
+
+new app();
